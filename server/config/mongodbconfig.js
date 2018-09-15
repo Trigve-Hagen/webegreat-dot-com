d7e34f23de7d860cb29d788445fda25548798b43
root <root@LAPTOP-NRRO2PQO.localdomain>
2018-09-14 21:41:07 -0700

    Woops thats my config

--- server/config/mongodbconfig.js
+++ server/config/mongodbconfig.js
@@ -4,12 +4,12 @@ module.exports = {
   db: 'mongodb://username:password@url:port/db',
   db_dev: 'mongodb://localhost:27017/github_db',
 
-  site_url: 'http://youdiful.com',
+  site_url: '',
   site_url_dev: 'http://localhost:9000',
 
-  facebook_app_id: '1834984053205833',
+  facebook_app_id: '',
   facebook_version: 'v3.1',
 
-  github_client_id: '967c6c6f5db09ccedb0f',
-  github_client_secret: 'b90f1aba403bc79a5b2c48afc2d3cd28a3de8b2c',
+  github_client_id: '',
+  github_client_secret: '',
 };
