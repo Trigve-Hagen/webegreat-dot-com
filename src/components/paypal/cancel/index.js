64fdd38810c0f348f25a803ddd127a9f93ed558a
Trigve Hagen <trigve.hagen@gmail.com>
2018-10-24 11:08:04 -0700

    Updated the success and cancel pages for Paypal. Testing the payflow in the sandbox.

--- src/components/paypal/cancel/index.js
+++ src/components/paypal/cancel/index.js
@@ -9,9 +9,24 @@ class Cancel extends React.Component {
         super(props);
         this.state = {
             paypalCancelError: '',
-            paypalPaymentId: ''
+            paypalPaymentId: '',
+            windowHeight: 0,
+            footerHeight: 0,
+            menuHeight: 0
         }
     }
+
+    componentDidMount() {
+		//console.log(window.innerHeight + ", " + window.clientHeight + ", " + window.height)
+        let windowHeight = isNaN(window.innerHeight) ? window.clientHeight : window.innerHeight;
+        let footerHeight = document.getElementsByClassName('webegreat-footer')[0].clientHeight;
+        let menuHeight = document.getElementsByClassName('webegreat-menu')[0].clientHeight;
+        this.setState({
+            windowHeight: windowHeight,
+            footerHeight: footerHeight,
+            menuHeight: menuHeight
+		});
+	}
 
     componentDidMount() {
 		fetch(config.site_url + '/api/paypal/cancel', {
@@ -37,14 +52,20 @@ class Cancel extends React.Component {
     }
 
     render() {
+        let containerHeight = this.state.windowHeight - (this.state.menuHeight + this.state.footerHeight);
         return (
             <div>
                 <Navigation path="/cancel" authenticated={this.props.authentication[0].authenticated}/>
-                <div className="container">
+                <div
+                    className="container"
+                    style={{
+                        minHeight: containerHeight + 'px'
+                    }}
+                >
                     <div className="row margin-top-20px margin-bottom-50px">
                         <div className="col-lg-12 col-md-12 col-sm-12 col xs-24">
                             <h4>Please come back and shop with us again.</h4>
-                            <p>There is a 100% money back garuntee if for any reason you are not satisfied with your products. Just return the item in the condition you recieved it to<br /><br />Returns<br />13066 Paddy Creek Ln<br />Lodi, CA 95240.</p>
+                            <p>There is a 100% money back guarantee if for any reason you are not satisfied with your products. Just return the item in the condition you received it to<br /><br /><h4>Returns</h4><br />13066 Paddy Creek Ln<br />Lodi, CA 95240.</p>
                             {
                                 (this.state.paypalCancelError) ? (
                                     <label>{this.state.paypalCancelError}</label>
--- src/components/paypal/success/index.js
+++ src/components/paypal/success/index.js
@@ -13,9 +13,24 @@ class Success extends React.Component {
         super(props);
         this.state = {
             paypalSuccessError: '',
-            paypalSuccessResults: ''
+            paypalSuccessResults: '',
+            windowHeight: 0,
+            footerHeight: 0,
+            menuHeight: 0
         }
     }
+
+    componentDidMount() {
+		//console.log(window.innerHeight + ", " + window.clientHeight + ", " + window.height)
+        let windowHeight = isNaN(window.innerHeight) ? window.clientHeight : window.innerHeight;
+        let footerHeight = document.getElementsByClassName('webegreat-footer')[0].clientHeight;
+        let menuHeight = document.getElementsByClassName('webegreat-menu')[0].clientHeight;
+        this.setState({
+            windowHeight: windowHeight,
+            footerHeight: footerHeight,
+            menuHeight: menuHeight
+		});
+	}
 
     componentDidMount() {
 		fetch(config.site_url + '/api/paypal/success', {
@@ -44,39 +59,48 @@ class Success extends React.Component {
 
     render() {
         //console.log(this.props.location.search.split("?")[1].split("&")[2].split("=")[1]);
+        let containerHeight = this.state.windowHeight - (this.state.menuHeight + this.state.footerHeight);
         return (
             <div>
                 <Navigation path="/success" authenticated={this.props.authentication[0].authenticated}/>
-                <div className="container">
-                    <div className="row margin-top-20px margin-bottom-50px">
-                        <div className="col-lg-8 col-md-8 col-sm-12 col xs-24">
-                            <h3>Thank you {this.state.paypalSuccessResults} for shopping with us.</h3>
-                            <p>Please allow up to 6 weeks for shipping.</p>
-                            {
-                                (this.state.paypalSuccessError) ? (
-                                    <label>{this.state.paypalSuccessError}</label>
-                                ) : (null)
-                            }
-                            {
-                                sort(this.props.cart).map( item =>
-                                    <div className="col-lg-12 col-md-12 col-sm-12 col-xs-24 margin-bottom-20px" key={item.id}>
-                                        <div className="col-lg-4 col-md-4 col-sm-12 col-xs-24">
-                                            <img src={ `/img/products/${item.image}` } alt={item.name} className="img-responsive margin-center" />
-                                        </div>
-                                        <div className="col-lg-8 col-md-8 col-sm-12 col-xs-24 padding-top-20px text-center">
-                                            <h2 className="zero-space-bottom">{item.name}</h2>
-                                            <p className="zero-space-top">{item.description}</p>
-                                            <h2 className="zero-space-bottom"></h2>
-                                            <h2 className="zero-space-bottom">{item.quantity} @ ${ item.price * item.quantity }</h2>
-                                        </div>
-                                    </div>
-                                )
-                            }
-                            
-                        </div>
-                        <div className="col-lg-4 col-md-4 col-sm-12 col xs-24">
-                            <h4>Please come back and<br />shop with us again.</h4>
-                            <p>There is a 100% money back garuntee if for any reason you are not satisfied with your products. Just return the item in the condition you recieved it to<br /><br />Returns<br />13066 Paddy Creek Ln<br />Lodi, CA 95240.</p>
+                <div
+                    className="container"
+                    style={{
+                        minHeight: containerHeight + 'px'
+                    }}
+                >
+                    <div className="row">
+                        <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                            <div className="row margin-top-20px margin-bottom-50px">
+                                <div className="col-lg-8 col-md-8 col-sm-12">
+                                    <h3>Thank you {this.state.paypalSuccessResults} for shopping with us.</h3>
+                                    <p>Please allow up to 6 weeks for shipping.</p>
+                                    {
+                                        (this.state.paypalSuccessError) ? (
+                                            <label>{this.state.paypalSuccessError}</label>
+                                        ) : (null)
+                                    }
+                                    {
+                                        sort(this.props.cart).map( item =>
+                                            <div className="col-lg-12 col-md-12 col-sm-12 margin-bottom-20px" key={item.id}>
+                                                <div className="col-lg-4 col-md-4 col-sm-12 col-xs-24">
+                                                    <img src={ `/img/products/${item.image}` } alt={item.name} className="img-responsive margin-center" />
+                                                </div>
+                                                <div className="col-lg-8 col-md-8 col-sm-12 padding-top-20px text-center">
+                                                    <h2 className="zero-space-bottom">{item.name}</h2>
+                                                    <p className="zero-space-top">{item.description}</p>
+                                                    <h2 className="zero-space-bottom"></h2>
+                                                    <h2 className="zero-space-bottom">{item.quantity} @ ${ item.price * item.quantity }</h2>
+                                                </div>
+                                            </div>
+                                        )
+                                    }
+                                </div>
+                                <div className="col-lg-4 col-md-4 col-sm-12 col xs-24">
+                                    <h4>Please come back and<br />shop with us again.</h4>
+                                    <p>There is a 100% money back garuntee if for any reason you are not satisfied with your products. Just return the item in the condition you recieved it to<br /><br /><h4>Returns</h4><br />13066 Paddy Creek Ln<br />Lodi, CA 95240.</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
